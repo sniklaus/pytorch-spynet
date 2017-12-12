a0935f012c69c6c4395fd4c3a0d5e0fb2338629e
sniklaus <simon.niklaus@outlook.com>
2017-12-12 15:35:05 -0800

    no message

--- run.py
+++ run.py
@@ -130,7 +130,7 @@ class Network(torch.nn.Module):
 
 		self.modulePreprocess = Preprocess()
 
-		self.moduleBasic = torch.nn.ModuleList([ Basic(intLevel).cuda() for intLevel in range(6) ])
+		self.moduleBasic = torch.nn.ModuleList([ Basic(intLevel) for intLevel in range(6) ])
 
 		self.moduleBackward = Backward()
 	# end
