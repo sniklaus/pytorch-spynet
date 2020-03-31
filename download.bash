e15556a3cacb4188348204cec750c9a14879f2dc
Simon Niklaus <simon.niklaus@outlook.com>
2020-03-31 09:54:57 -0700

    no message

--- download.bash
+++ download.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-chairs-clean.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-chairs-final.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-kitti-final.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-sintel-clean.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-sintel-final.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-chairs-clean.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-chairs-final.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-kitti-final.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-sintel-clean.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-spynet/network-sintel-final.pytorch
